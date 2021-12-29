e29f147cd6f3cc963ac70f90ceafabc5b97d5e39
GregoryConrad <gregorysconrad@gmail.com>
2021-12-29 18:06:33 -0500

    Updated worker for v3

--- src/worker.ts
+++ src/worker.ts
@@ -1,13 +1,7 @@
-// This should be a service worker, but:
-// https://bugs.chromium.org/p/chromium/issues/detail?id=1024211
 chrome.webRequest.onCompleted.addListener(async ({ url }) => {
     try {
-        // Commented out because we had to downgrade to manifest v2
-        // const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
-        // chrome.tabs.sendMessage(tab.id!, url)
-        chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
-            chrome.tabs.sendMessage(tab.id!, url)
-        })
+        const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+        chrome.tabs.sendMessage(tab.id!, url)
     } catch (exception) {
         console.log('Failed to notify the curent tab of the request url', exception)
     }
